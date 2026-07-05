2709b33775f98aa51d5093bea04611ecec3fcfdf
Johannes (jotrorox) <mail@jotrorox.com>
2026-07-05 19:40:19 +0200

    simplest version

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.26.4
 
 require (
 	github.com/go-chi/chi/v5 v5.3.0
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.12.3
 )
--- main.go
+++ main.go
@@ -2,17 +2,57 @@ package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
 	_ "github.com/lib/pq"
+
+	"github.com/joho/godotenv"
 )
 
+type Puzzle struct {
+	ID     string `json:"id"`
+	FEN    string `json:"fen"`
+	Moves  string `json:"moves"`
+	Rating int    `json:"rating"`
+}
+
+func getRandomPuzzle(db *sql.DB) (*Puzzle, error) {
+	//                                                    Table "public.lichess_puzzles"
+	//       Column      |           Type           | Collation | Nullable | Default  | Storage  | Compression | Stats target | Description
+	// ------------------+--------------------------+-----------+----------+----------+----------+-------------+--------------+-------------
+	//  puzzle_id        | text                     |           | not null |          | extended |             |              |
+	//  fen              | text                     |           | not null |          | extended |             |              |
+	//  moves            | text                     |           | not null |          | extended |             |              |
+	//  rating           | integer                  |           | not null |          | plain    |             |              |
+	// Indexes:
+	//     "lichess_puzzles_pkey" PRIMARY KEY, btree (puzzle_id)
+	//     "lichess_puzzles_rating_idx" btree (rating)
+
+	var puzzle Puzzle
+	err := db.QueryRow("SELECT puzzle_id, fen, moves, rating FROM lichess_puzzles ORDER BY RANDOM() LIMIT 1").Scan(
+		&puzzle.ID, &puzzle.FEN, &puzzle.Moves, &puzzle.Rating)
+	if err != nil {
+		return nil, err
+	}
+
+	return &puzzle, nil
+}
+
 func main() {
-	db, err := sql.Open()
+	_ = godotenv.Load()
+
+	db_url, ok := os.LookupEnv("DB_URL")
+	if !ok {
+		log.Fatal("DB_URL not set")
+	}
+	db, err := sql.Open("postgres", db_url)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,8 +68,26 @@ func main() {
 
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World!"))
+	r.Use(middleware.Compress(5, "application/json"))
+	r.Use(middleware.Timeout(time.Second * 30))
+
+	r.Get("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
 	})
+
+	r.Get("/puzzle", func(w http.ResponseWriter, r *http.Request) {
+		puzzle, err := getRandomPuzzle(db)
+
+		if err != nil {
+			log.Println("Error getting random puzzle:", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(puzzle)
+	})
+
 	http.ListenAndServe(":3000", r)
 }
